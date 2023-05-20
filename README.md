f359f9139a8b3f21efe74eeddf745acd68e2af49
Kyle Demeule <kyle.demeule@gmail.com>
2023-05-19 17:33:13 -0700

    Basic flow that extracts runs from strava.

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # running-with-prefect
+
+## Pyenv
+
+```
+pyenv install 3.10.11
+pyenv virtualenv 3.10.11 rwp
+pyenv activate rwp
+pip install -r requirements.txt
+```
---
+++ flows/sync_activities.py
@@ -0,0 +1,111 @@
+import argparse
+import datetime
+import json
+import requests
+
+from prefect import flow, task, get_run_logger
+from prefect.blocks.system import Secret
+
+STRAVA_MAX_PAGES=100
+STRAVA_PAGE_SIZE=100
+
+@task
+def parse_dates(start_date, end_date):
+  if start_date:
+    start_date = datetime.datetime.strptime(start_date, '%Y-%m-%d')
+  else:
+    # default to 3 days
+    start_date = datetime.datetime.combine(datetime.datetime.now() - datetime.timedelta(days=3), datetime.datetime.min.time())
+
+  if end_date:
+    end_date = datetime.datetime.strptime(end_date, '%Y-%m-%d')
+  else:
+    # default to tomorrow
+    end_date = datetime.datetime.combine(datetime.datetime.now() + datetime.timedelta(days=1), datetime.datetime.min.time())
+
+  return start_date, end_date
+
+@task
+def get_strava_token():
+  strava_client_id = Secret.load("strava-client-id").get()
+  strava_secret = Secret.load("strava-secret").get()
+  strava_refresh_token = Secret.load("strava-refresh-token").get()
+
+  r = requests.post(
+    "https://www.strava.com/oauth/token",
+    data={
+      "client_id": strava_client_id,
+      "client_secret": strava_secret,
+      "refresh_token": strava_refresh_token,
+      "grant_type": "refresh_token",
+    }
+  )
+  r.raise_for_status()
+  tokens = json.loads(r.text)
+  return tokens["access_token"]
+
+@task
+def fetch_activities_from_strava(strava_token, start_date, end_date):
+  logger = get_run_logger()
+  logger.info("Starting with params {}, {}".format(start_date, end_date))
+
+  activities = []
+  for page_i in range(1, STRAVA_MAX_PAGES + 1):
+    r = requests.get(
+      "https://www.strava.com/api/v3/athlete/activities",
+      headers={"Authorization": "Bearer {}".format(strava_token)},
+      params={
+        "after": int(start_date.timestamp()),
+        "before": int(end_date.timestamp()),
+        "page": page_i,
+        "per_page": STRAVA_PAGE_SIZE
+      }
+    )
+    r.raise_for_status()
+    page_activities = json.loads(r.text)
+    for a in page_activities:
+      if a.get("type") == "Run":
+        activities.append(
+          {
+            "distance": a.get("distance"),
+            "elapsed_time": a.get("elapsed_time"),
+            "id": a.get("id"),
+            "moving_time": a.get("moving_time"),
+            "start_date": a.get("start_date"),
+            "total_elevation_gain": a.get("total_elevation_gain"),
+          }
+        )
+      else:
+        pass
+    if len(page_activities) == 0:
+      break
+  else:
+    raise RuntimeError("Reached max page {} but still not finished.".format(STRAVA_MAX_PAGES))
+
+  return activities
+
+@task
+def upsert_activities_to_bigquery(activities):
+  logger = get_run_logger()
+  logger.info("Started with {} activities to upsert.".format(len(activities)))
+
+  with open("ignore/activities.json", 'w') as file_object:
+    json.dump(activities, file_object)
+  return
+
+@flow
+def sync_activities(start_date=None, end_date=None):
+  start_date, end_date = parse_dates(start_date, end_date)
+  strava_token = get_strava_token()
+  activities = fetch_activities_from_strava(strava_token, start_date, end_date)
+  upsert_activities_to_bigquery(activities)
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--start_date", help="", action="store", required=False)
+parser.add_argument("--end_date", help="", action="store", required=False)
+if __name__=="__main__":
+  args = parser.parse_args()
+  sync_activities(
+    start_date=args.start_date,
+    end_date=args.end_date
+  )
